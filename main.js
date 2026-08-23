ff690e2dbc78122012cc0a0b1466907dcf6e4cc5
jaleel <djkm37594@gmail.com>
2026-08-23 18:16:54 +0100

    initial website

---
+++ main.js
@@ -0,0 +1,17 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background-color: #f5f1e8;
+    margin: 0;
+    padding: 50px;
+}
+
+h1 {
+    color: #2f5d50;
+    font-size: 42px;
+}
+
+p {
+    font-size: 20px;
+    color: #333;
+}
